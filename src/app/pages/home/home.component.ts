53c8234c60d5648dcb649e247ff1561b9fe5c4f9
Elias Fares <eliasferes420@gmail.com>
2025-05-06 22:56:40 +0300

    Routing between the pages , home page layout

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+   // Example station data (you can replace it with actual data)
+   stations = ['New York', 'Los Angeles', 'San Francisco', 'Chicago', 'Miami'];
 
+   search(){
+    const from = (document.getElementById('station-from') as HTMLSelectElement).value;
+    const to = (document.getElementById('station-to') as HTMLSelectElement).value;
+    const date = (document.getElementById('travel-date') as HTMLInputElement).value;
+
+    console.log(`Searching flights from ${from} to ${to} on ${date}`);
+   }
 }
